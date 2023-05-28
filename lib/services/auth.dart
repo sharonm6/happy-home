c1df08225d55d74efe42c68def197c0fbaee2ee4
sharonm6 <sharon.m0626@gmail.com>
2023-05-27 21:55:41 -0700

    Auto create new db docs when user logs in on new day

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -26,6 +26,21 @@ class AuthService {
       firebase_auth.UserCredential result = await _auth
           .signInWithEmailAndPassword(email: email, password: password);
       firebase_auth.User? user = result.user;
+
+      if (user != null &&
+          user!.metadata != null &&
+          user!.metadata!.lastSignInTime != null) {
+        if (user!.metadata!.lastSignInTime!.isBefore(DateTime.now())) {
+          await DatabaseService(uid: user!.uid)
+              .createMealLog(user!.uid, DateTime.now());
+          await DatabaseService(uid: user!.uid)
+              .createWaterLog(user!.uid, DateTime.now());
+          await DatabaseService(uid: user!.uid)
+              .createFitnessLog(user!.uid, DateTime.now());
+          await DatabaseService(uid: user!.uid)
+              .createPeriodLog(user!.uid, DateTime.now());
+        }
+      }
       return user;
     } catch (error) {
       print(error.toString());
