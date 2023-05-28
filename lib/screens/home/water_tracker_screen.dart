ba66cd73a931f2c5aaf95a43dff7a2c80de742d3
sharonm6 <sharon.m0626@gmail.com>
2023-05-28 00:20:45 -0700

    Implement Water Tracker UI
    Co-authored-by: Paris Haraguchi <pharaguc@uci.edu>

--- lib/screens/home/water_tracker_screen.dart
+++ lib/screens/home/water_tracker_screen.dart
@@ -26,6 +26,8 @@ class _WaterTrackerScreenState extends State<WaterTrackerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String? waterPotImg;
+
     return StreamBuilder<List<WaterLog>>(
         stream: databaseService.waterLog,
         builder: (context, mealSnapshot) {
@@ -33,30 +35,72 @@ class _WaterTrackerScreenState extends State<WaterTrackerScreen> {
             List<WaterLog> waterInfos = mealSnapshot.data!;
             WaterLog _waterLog = waterInfos[0];
 
+            if (_waterLog.cupsDrank >= 8) {
+              waterPotImg = "assets/water_pot_8.png";
+            } else if (_waterLog.cupsDrank >= 7) {
+              waterPotImg = "assets/water_pot_7.png";
+            } else if (_waterLog.cupsDrank >= 6) {
+              waterPotImg = "assets/water_pot_6.png";
+            } else if (_waterLog.cupsDrank >= 5) {
+              waterPotImg = "assets/water_pot_5.png";
+            } else if (_waterLog.cupsDrank >= 4) {
+              waterPotImg = "assets/water_pot_4.png";
+            } else if (_waterLog.cupsDrank >= 3) {
+              waterPotImg = "assets/water_pot_3.png";
+            } else if (_waterLog.cupsDrank >= 2) {
+              waterPotImg = "assets/water_pot_2.png";
+            } else if (_waterLog.cupsDrank >= 1) {
+              waterPotImg = "assets/water_pot_1.png";
+            } else {
+              waterPotImg = "assets/water_pot_0.png";
+            }
+
             return Column(children: [
-              ListTile(
-                leading: CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.brown[400],
-                ),
-                title: Text(_waterLog.date.toString()),
-                subtitle: Text(
-                  '${_waterLog.uid} uid\n'
-                  '${_waterLog.date} date\n'
-                  '${_waterLog.cupsDrank} cups drank\n'
-                  '${_waterLog.numDaysOld} numDaysOld\n'
-                  '${_waterLog.agedUp} agedUp\n',
+              SizedBox(height: 8),
+              Center(
+                child: Text(
+                  "${_waterLog.numDaysOld} Day(s) Old",
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
-              TextButton(
-                  onPressed: () async => {
-                        await DatabaseService().updateWaterLogData(
-                            widget._uid, DateTime.now(),
-                            numCupsAdd: 1)
-                      },
-                  child: Text(
-                    "Toggle cupsDrank: ${_waterLog?.cupsDrank}",
-                  )),
+              SizedBox(height: 8),
+              Image.asset(waterPotImg ?? 'assets/water_pot_0.png', height: 500),
+              SizedBox(height: 8),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Column(children: [
+                  IconButton(
+                    icon: Image.asset('assets/water_cup_half.png'),
+                    iconSize: 100,
+                    onPressed: () async => {
+                      await DatabaseService().updateWaterLogData(
+                          _waterLog.uid, DateTime.now(),
+                          numCupsAdd: 0.5)
+                    },
+                  ),
+                  Text("Drank 1/2 cup", style: TextStyle(fontSize: 18)),
+                ]),
+                Column(children: [
+                  IconButton(
+                    icon: Image.asset('assets/water_cup_full.png'),
+                    iconSize: 100,
+                    onPressed: () async => {
+                      await DatabaseService().updateWaterLogData(
+                          _waterLog.uid, DateTime.now(),
+                          numCupsAdd: 1)
+                    },
+                  ),
+                  Text("Drank 1 cup", style: TextStyle(fontSize: 18)),
+                ]),
+              ])
+              // TextButton(
+              //     onPressed: () async => {
+              //           await DatabaseService().updateWaterLogData(
+              //               _waterLog.uid, DateTime.now(),
+              //               numCupsAdd: 1)
+              //         },
+              //     child: Text(
+              //       "Toggle cupsDrank: ${_waterLog?.cupsDrank}",
+              //     )),
             ]);
           } else {
             return SizedBox(height: 0);
